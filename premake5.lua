d62a89c89d1243d481ce78b536e1f8fb866cb77c
TheCherno <y@nchernikov.com>
2021-04-15 14:41:50 +1000

    staticruntime off

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "Box2D"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++11"
-	staticruntime "on"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
