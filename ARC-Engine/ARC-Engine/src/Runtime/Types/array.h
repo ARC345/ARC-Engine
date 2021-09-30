a6c61fcb8bee18c294dd1019ab79dada27049b1e
Arnav_Rastogi <arnavrastogi.543@gmail.com>
2021-10-01 00:01:26 +0530

    hm

--- ARC-Engine/ARC-Engine/src/Runtime/Types/array.h
+++ ARC-Engine/ARC-Engine/src/Runtime/Types/array.h
@@ -8,17 +8,30 @@ namespace ARC
 		struct FArray_Base : public FContainer{};
 	}
 
-	template <typename T = float>
+	template <typename T>
 	struct FArray : public FArray_Base
 	{
 		/*--------------FUNCS--------------*/
 		public:
-		protected:
+			FArray(size_t p_cap = 5u) : m_Cap(p_cap);
+			~FArray();
+			FArray(const FArray& p_obj)
 
-			constexpr size_t GetSize() const override {
+			T& operator[](const size_t p_index)
+
+			void Init(const T& p_from);
+			void Init(const size_t& p_from);
+
+			void Expand();
+			void Push();
+
+			void Pop(const size_t& p_index);
+			void Remove();
+
+			virtual __forceinline const size_t& GetSize() const override {
 				return m_Len;
 			}
-
+		protected:
 		private:
 		/*---------------------------------*/
 		
@@ -27,6 +40,8 @@ namespace ARC
 		protected:
 		private:
 			size_t m_Len;
+			size_t m_Cap;
+			T** m_Arr;
 		/*---------------------------------*/
 	};
 };
