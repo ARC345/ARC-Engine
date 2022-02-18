d9cd79580f4e8fcef891045dcc163d6f7a3c8c85
Arnav_Rastogi <arnavrastogi.543@gmail.com>
2022-02-18 21:49:47 +0530

    conflicts removed

--- ARC-Engine/Sandbox/src/Sandbox2D.cpp
+++ ARC-Engine/Sandbox/src/Sandbox2D.cpp
@@ -24,7 +24,7 @@ CSandbox2D::CSandbox2D() :
 
 void CSandbox2D::OnAttach()
 {
-	ARC::HPR::INI::File inif();
+	CIniFile inif();
 	m_CheckerboardTexture = ARC::CTexture2D::Create("assets/textures/Checkerboard.png");
 }
 
