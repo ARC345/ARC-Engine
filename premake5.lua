70638477ae163d72610c2573ee6d669ccc72b4d1
Arnav_Rastogi <arnavrastogi.543@gmail.com>
2023-03-27 16:01:14 +0530

    premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,10 @@
 project "Box2D"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++11"
 	staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("_bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("_int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
